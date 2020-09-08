a699df285234ce440e1ba8dcc55c86faf37e484f
LatHalim <hamitovich86@mail.ru>
2020-09-08 19:51:09 +0300

    16-2,17-2

--- js/ts16.js
+++ js/ts16.js
@@ -1,73 +1,61 @@
 /* Задача №1*/
-let masiv = ['first', 5, true];
-  console.log(masiv);
-
+let massiv = ['first', 5, true];
+document.getElementById('massiv').innerHTML = massiv;
 
 /* Задача №2*/
 a3 = {
-    3 : 'hello',
-    'one' : 'hi',
-    'testt' : 'vodoley',
-    'ivan' : 'ivanov'
-    }
-    let two = (Object.keys(a3));
-    let resTwo = two.filter(i => i.length > 4);
-    console.log(resTwo)
+  3: 'hello',
+  'one': 'hi',
+  'testt': 'vodoley',
+  'ivan': 'ivanov'
+}
+let two = (Object.keys(a3));
+let resTwo = two.filter(i => i.length > 4);
+document.getElementById('resTwo').innerHTML = resTwo;
 
 /* Задача №3*/
-let newMassiv = new Map();
-console.log(newMassiv);
-function acoMassiv () {
+let addNewMassiv = new Map();
+function addMassiv() {
   let key = document.getElementById('u7-key_input');
   let value = document.getElementById('u7-value_input');
-  newMassiv.set(key.value,value.value);
-  console.log(newMassiv);
+  addNewMassiv.set(key.value, value.value);
+  console.log(addNewMassiv);
 }
 
+let addNewMassiv2 = {};
+function addMassiv2() {
+  let key = document.getElementById('u7-key_input2');
+  let value = document.getElementById('u7-value_input2');
+  addNewMassiv2[key.value]=value.value;
+  console.log(addNewMassiv2);
+}
 
 /* Задача №4*/
-let a = ['Tor', 'Lokki', 'Odin',34, 'privet'];
-let four = (a.length);
-console.log(four)
-
+let a = ['Tor', 'Lokki', 'Odin', 34, 'privet'];
+let arr = (a.length);
+document.getElementById('arr').innerHTML = arr;
 
 /* Задача №5*/
-d = [0, 2, 5, -4, 6, 22, -9, -12, ,13, 78]
+d = [0, 2, 5, -4, 6, 22, -9, -12, , 13, 78]
 let a11 = document.getElementById('a1');
 let a22 = document.getElementById('a2');
 let a1 = [];
 let a2 = [];
-    for (i = 0; i < d.length; i++)
-{
-  if ((d [i] % 2) === 0)
-  {
-      a1.push(d[i]);
-      a11.innerHTML = ("Четные: " + a1);
+for (i = 0; i < d.length; i++) {
+  if ((d[i] % 2) === 0) {
+    a1.push(d[i]);
+    a11.innerHTML = ("Четные: " + a1);
   }
-  if ((d [i] % 2) === 1)
-  {
+  if (d[i] % 2) {
     a2.push(d[i]);
-      a22.innerHTML = ("Нечетные: " + a2);
-  }
-  if ((d [i] % 2) === -1)
-  {
-    a2.push(d[i]);
-      a22.innerHTML = ("Нечетные: " + a2);
+    a22.innerHTML = ("Нечетные: " + a2);
   }
 }
 
-
 /* Задача №6*/
 d = [0, 2, 5, -4, 6, 22, -9, -12, 13, 78]
-let res = 1;
+let res = 0;
 for (i = 0; i < d.length; i++) {
-  res = res+d[i];
-  }
-  console.log(res);
-
-
-
-
-
-
-
+  res = res + d[i];
+}
+document.getElementById('res').innerHTML = res;
--- js/ts17.js
+++ js/ts17.js
@@ -6,38 +6,18 @@ function name() {
 }
 document.getElementById('submit').onclick = name;
 
-
 /* Задача №2*/
-function abc(a, b) {
-    if (a > b) {
-        console.log(a)
-    }
-    if (a <= b) {
-        console.log(b)
-    }
+function max(a, b) {
+    return a > b ? a : b;
 }
-let result = abc(4, 7)
-
-
-/* Задача №3*/
-function abC(d, c) {
-    if (d > c) {
-        console.log(d)
-    }
-    if (d <= c) {
-        console.log(c)
-    }
-}
-let result1 = abC(5, 9)
-
-
+document.getElementById('result').innerHTML = max(2, 4)
 
 /* Задача №4*/
 function randomMM(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-let resultRan = randomMM(1, 100)
+let resultRan = randomMM(1, 3)
 Random = document.getElementById('Random')
 Random.innerHTML = resultRan;
